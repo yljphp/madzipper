5d2a4b13394cd5d847409418404e9ca9d30f6662
yljphp <yljphp@gmail.com>
2023-05-16 17:04:47 +0800

    first

---
+++ README.md
@@ -0,0 +1,8 @@
+#Note
+
+
+``` bash
+composer require Yljphp/madzipper
+```
+
+修复了原始扩展包在PHP7下的bug
---
+++ tests/TestCase.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Yljphp\Madzipper\Tests;
+
+use Yljphp\Madzipper\MadzipperServiceProvider;
+// use Mockery;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+abstract class TestCase extends Orchestra
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // Debug helper
+        // Mockery::setLoader(new Mockery\Loader\RequireLoader(sys_get_temp_dir()));
+    }
+
+    /**
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            MadzipperServiceProvider::class,
+        ];
+    }
+}
